818313b537a3dcf95fc8abdc7ee9abe40faf2b4c
Vita <75259415+Vitacci@users.noreply.github.com>
2024-03-24 15:51:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,58 +1,68 @@
 // Copyright 2021 NNTU-CS
+int findFirstGreaterThan(int* arr, int size, int value) {
+  for (int i = 0; i < size; ++i) {
+    if (arr[i] > value) {
+      return i;
+    }
+  }
+  return 0;
+}
+int binarySearch(int* arr, int size, int value) {
+  int found = 0;
+  int first = 0;
+  int last = size - 1;
+  int middle = 0;
+  while (first <= last) {
+    middle = (first + last) / 2;
+    if (arr[middle] == value) {
+      ++found;
+      int index = middle - 1;
+      while (index >= first && arr[index] == value) {
+        ++found;
+        --index;
+      }
+      index = middle + 1;
+      while (index <= last && arr[index] == value) {
+        ++found;
+        ++index;
+      }
+      return found;
+    } else if (arr[middle] < value) {
+      first = middle + 1;
+    } else {
+      last = middle - 1;
+    }
+  }
+  return 0;
+}
 int countPairs1(int *arr, int len, int value) {
-  int cnt1 = 0;
-  for (int i = 0; i < len; i++) {
-    for (int j = i+1; j < len; j ++) {
-      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
-        cnt1 += 1;
+  int result = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (value == arr[i] + arr[j]) {
+        ++result;
       }
     }
   }
-  return cnt1;
+  return result;
 }
-
 int countPairs2(int *arr, int len, int value) {
-  int cnt2 = 0;
-  int r = len - 1;
-  while (arr[r] > value)
-    r--;
-  for (int i = 0; i < r; i++) {
-    for (int j = r; j > i; j--) {
-      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
-        cnt2 += 1;
-      } else if (arr[i] + arr[j] < value) {
-        break;
+  int result = 0;
+  len = findFirstGreaterThan(arr, len, value);
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (value == arr[i] + arr[j]) {
+        ++result;
       }
     }
   }
-  return cnt2;
+  return result;
 }
-
 int countPairs3(int *arr, int len, int value) {
-  int cnt3 = 0;
-  for (int i = 0; i < len - 1; i++) {
-    int l = i;
-    int r = len;
-    while (l < r - 1) {
-      int centre = (r + l) / 2;
-      if (arr[i] + arr[centre] == value) {
-        cnt3 += 1;
-        int temp = centre + 1;
-        while ((arr[i] + arr[temp] == value) && (temp < r)) {
-          cnt3++;
-          temp++;
-        }
-        while ((arr[i] + arr[temp] == value) && (temp > l)) {
-          cnt3++;
-          temp--;
-        }
-        break;
-      } else if (arr[i] + arr[centre] > value) {
-        r = centre;
-      } else {
-        l = centre;
-      }
-    }
+  int result = 0;
+  for (int i = 0; i < len; ++i) {
+    int n = value - arr[i];
+    result += binarySearch(arr + i + 1, len - i - 1, n);
   }
-  return cnt3;
+  return result;
 }
